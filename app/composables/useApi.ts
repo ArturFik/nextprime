0d2549cc8029727090ef2f95962d965bd364b14a
ArturFik <arturnike44@mail.ru>
2026-06-25 17:14:30 +0500

    add

---
+++ app/composables/useApi.ts
@@ -0,0 +1,27 @@
+// composables/useApi.ts
+export const useApi = () => {
+  const API_URL = "https://nextprime-three.vercel.app/"; // или твой домен
+
+  const getUserData = async (telegramId: number) => {
+    const response = await fetch(
+      `${API_URL}/api/user?telegram_id=${telegramId}`
+    );
+    return response.json();
+  };
+
+  const getTodayWorkout = async (telegramId: number) => {
+    const response = await fetch(
+      `${API_URL}/api/workouts/today?telegram_id=${telegramId}`
+    );
+    return response.json();
+  };
+
+  const getTodayNutrition = async (telegramId: number) => {
+    const response = await fetch(
+      `${API_URL}/api/nutrition/today?telegram_id=${telegramId}`
+    );
+    return response.json();
+  };
+
+  return { getUserData, getTodayWorkout, getTodayNutrition };
+};
